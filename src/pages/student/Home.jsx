06a47c280a00e7c0bf30a1d1a504947325627451
Phúc Nguyễn Bá <nbphucss@gmail.com>
2025-11-03 12:31:11 +0700

    Update student-ui-component: CallToAction, Footer

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Companies from '../../components/student/Companies'
 import CourseSection from '../../components/student/CoursesSection'
+import CallToAction from '../../components/student/CallToAction'
+import Footer from '../../components/student/Footer'
 
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Companies/>
       <CourseSection/>
+      <CallToAction/>
+      <Footer/>
     </div>
   )
 }
