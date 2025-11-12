ecb7c839f8163f7d249c9615074ebe348ec6b1f5
phunh1901 <phunh1901@gmail.com>
2025-11-12 19:56:59 +0700

    Keep my local code after resolving merge conflict

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -1,21 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react'
-import Companies from '../../components/student/Companies'
-import CourseSection from '../../components/student/CoursesSection'
-import CallToAction from '../../components/student/CallToAction'
-import Footer from '../../components/student/Footer'
-
-const Home = () => {
-  return (
-    <div className='flex flex-col items-center space-y-7 text-center'>
-      <Companies/>
-      <CourseSection/>
-      <CallToAction/>
-      <Footer/>
-    </div>
-  )
-}
-=======
 import React from "react";
 import Hero from "../../components/student/Hero";
 import TestimonialsSection from "../../components/student/TestimonialsSection";
@@ -28,6 +10,5 @@ const Home = () => {
     </div>
   );
 };
->>>>>>> Stashed changes
 
 export default Home;
