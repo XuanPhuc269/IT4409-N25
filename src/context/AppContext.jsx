d09de5ed6da0368f70cfbc769b267e16a3f81cea
XuanPhuc269 <tranxuanphuc269@gmail.com>
2025-12-24 09:58:49 +0700

    feat: Integrate user authentication and token logging in AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,11 +1,16 @@
 import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
+import { useAuth, useUser } from "@clerk/clerk-react";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
     const [allCourses, setAllCourses] = useState([])
+    
+    const {getToken} = useAuth();
+    const {user} = useUser();
+    
 
     const fetchAllCourses = async () => {
         setAllCourses(dummyCourses)
@@ -27,9 +32,23 @@ export const AppContextProvider = (props) => {
         fetchAllCourses(dummyCourses)
     }, [])
 
+    const logToken = async () => {
+        const token = await getToken();
+        console.log("Clerk Token: ", token);
+    }
+
+    useEffect(() => {
+        if (user) {
+            logToken();
+        } else {
+            console.log("No user signed in");
+        }
+    },[user])
+
     const value = {
         allCourses, calculateRating
     }
+
 
     return (
         <AppContext.Provider value={value}>
